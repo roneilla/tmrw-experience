237f4e383e80221ddf987614a3fe3b7dc44978da
Roneilla <bumanlar@sheridancollege.ca>
2020-03-02 09:46:04 -0500

    Reuploaded files
    for better organization/naming convention

--- Augmented Reality/test_1/scripts_original/playVideo.js
+++
@@ -1,6 +0,0 @@
-   function playVideo() {
-       if ((markerRoot1.visible === true)) {
-           coral.video.play();
-           pc++;
-       }
-   }
---
+++ Augmented Reality/test_2/lib/global.js
@@ -0,0 +1,21 @@
+  var renderer, scene, camera;
+  var arToolkitContext, onRenderFcts, arToolkitSource, markerRoot, lastTimeMsec;
+
+  var params = {
+      opacity: 1
+  };
+
+  var pc = 0;
+
+  // declare variables here
+
+//   var fullVideo = new MarkerVideo('../videos/FULL.mp4');
+  var imageTest = new MarkerImage('img/club62.png');
+  imageTest.setSize(2, 1, 1);
+  imageTest.setPosition(0, 0, 0);
+
+  //  fullVideo.setSize(1080, 1080);
+  //  fullVideo.load();
+
+  // marker variables
+  var marker1, marker2;
